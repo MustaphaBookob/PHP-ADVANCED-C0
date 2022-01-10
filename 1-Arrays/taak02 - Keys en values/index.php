a6e5f28ec19ba2510b0905292431624e1c562e09
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2022-01-10 11:20:10 +0100

    1.2

---
+++ 1-Arrays/taak02 - Keys en values/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$persoon["voornaam"]   = "Willem Alexander ";
+$persoon["achternaam"] = "Van oranje";
+$persoon["opleiding"]  = "software ontwikkeling";
+$persoon["leeftijd"]   = 53;
+
+echo  $persoon["voornaam"] . $persoon["achternaam"] . " is " . $persoon["leeftijd"] . "jaar oud en is student aan het ROC van Amsterdam aan de opleiding " . $persoon["opleiding"]; 
+
+?>
