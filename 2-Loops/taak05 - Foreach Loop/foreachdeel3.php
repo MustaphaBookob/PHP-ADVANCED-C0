4127a7f27374dc44ceb49a7bc96883e1c8ad50b0
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2022-01-17 11:47:57 +0100

    2-5

---
+++ 2-Loops/taak05 - Foreach Loop/foreachdeel3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$student = array("voornaam"=>"jan", "Achternaam"=>"Jansen", "Klas"=>"9A", "leeftijd"=>"17", "Woonplaats"=>"Amstelveen");
+
+foreach($student as $x => $val) {
+  echo "$x = $val<br>";
+}
+?>
+
+</body>
+</html>
+
+
+
+
+
+
+
