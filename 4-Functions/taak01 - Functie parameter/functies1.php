ec6fa6858b9df67c9c59370c5816d5227ad218ca
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2022-03-15 10:12:23 +0100

    4.1

---
+++ 4-Functions/taak01 - Functie parameter/functies1.php
@@ -0,0 +1,18 @@
+
+
+<?php
+schrijfkleur("red");
+schrijfkleur('green');
+schrijfkleur("blue");
+schrijfkleur("yellow");
+
+function schrijfKleur($kleur){
+
+    echo "<div style='color: $kleur; '> deze tekst heeft de kleur $kleur</div>";
+}
+
+
+
+?>
+</body>
+</html>
